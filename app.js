b6d9f326322bed1b55139d21c1abe24a8845f925
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-05-07 14:26:44 +0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 function timeKnow() {
+    let now = new Date();
     let daysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let now = new Date();
-alert('Today is: ' + daysName[now.getDay()]);
-alert('Current Time is: ' + (now.getHours() > 12 ? now.getHours() - 12 : now.getHours()) + ':' + now.getMinutes());
-
-}
+        let hour = now.getHours()
+        console.log(hour);
+        if(hour >= 12){
+            hour = hour + " PM"
+            hour = hour-12
+        }
+        else{
+            hour = hour + " AM"
+            
+        }
+        // console.log(hour);
+        // alert(daysName[now.getDay()])
+        alert('Today is: ' + daysName[now.getDay()]);
+        alert("Current Time Is:"+hour + " "+ now.getMinutes()+ " " + now.getSeconds());
+    }
+    
